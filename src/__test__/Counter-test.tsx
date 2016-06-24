9f09a023311b58bbb7196285e7a230a6553d4d6f
uryyyyyyy <koki305@gmail.com>
2016-06-24 22:32:38 +0900

    modify component test, for testing rendering

--- src/__test__/Counter-test.tsx
+++ src/__test__/Counter-test.tsx
@@ -2,31 +2,27 @@ import * as React from "react";
 import Counter from "../Counter";
 import {spy} from "sinon";
 import {assert} from "chai";
-import {MyAction, DispatchActions, ActionTypes} from "../Models";
+import {GlobalState} from "../Models";
 import * as TestUtils from "react-addons-test-utils";
 import * as ReactDOM from "react-dom";
 
 describe('Counter test', () => {
 
-    it('normal click test', () => {
-        const spyCB:any = spy();
-        const actions:DispatchActions = new DispatchActions(spyCB);
+    it('rendering test', () => {
+        const actions:any = {};
+        const state: GlobalState = {num: 1};
         const counterComponent: any = TestUtils.renderIntoDocument(
-            <Counter value={{num: 0}} actions={actions} />
+            <Counter value={state} actions={actions} />
         );
 
         const counterDOM = ReactDOM.findDOMNode(counterComponent);
-        const buttons: NodeListOf<HTMLButtonElement> = counterDOM.getElementsByTagName("button");
-        const button: HTMLButtonElement = buttons[1];
+        const ps: NodeListOf<HTMLParagraphElement> = counterDOM.getElementsByTagName("p");
+        const p: HTMLParagraphElement = ps[0];
 
-        TestUtils.Simulate.click(button);
-        assert.deepEqual(spyCB.calledOnce, true);
-
-        const decrement: MyAction = { type: ActionTypes.DECREMENT, amount: 2};
-        assert.deepEqual(spyCB.calledWith(decrement), true);
+        assert.deepEqual(p.textContent, "score: 1");
     });
 
-    it('click test2', () => {
+    it('click test', () => {
         const spyCB:any = spy();
         const actions:any = {fetchAmount: spyCB};
         const counterComponent: any = TestUtils.renderIntoDocument(
