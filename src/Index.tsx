3fcb8c6f3ffb42ed0d7c92dc64368b50a124e0c6
uryyyyyyy <koki305@gmail.com>
2016-07-30 10:49:19 +0900

    tiny-fix

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,5 +1,5 @@
-import React = require('react');
-import ReactDOM = require('react-dom');
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import Hello from "./Hello";
 
 ReactDOM.render(<Hello content="hello world"/>, document.getElementById('app'));
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -28,11 +28,6 @@ module.exports = {
     loaders: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
       { test: /\.tsx?$/, loader: "ts-loader" }
-    ],
-
-    preLoaders: [
-      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-      { test: /\.js$/, loader: "source-map-loader" }
     ]
   }
 };
