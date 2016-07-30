2bfae1a6e16b37d0bc22f0e864afd00e5df73c31
uryyyyyyy <koki305@gmail.com>
2016-07-30 10:21:06 +0900

    tsd -> typings & divide webpack prod/dev

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: "./src/Index.tsx",
+  output: {
+    filename: "./dist/bundle.js"
+  },
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ["", ".ts", ".tsx", ".js"]
+  },
+
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+
+  module: {
+    loaders: [
+      // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+      { test: /\.tsx?$/, loader: "ts-loader" }
+    ],
+
+    preLoaders: [
+      // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+      { test: /\.js$/, loader: "source-map-loader" }
+    ]
+  }
+};
